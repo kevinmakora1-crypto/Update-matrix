e3a24a2f38aac7fb9af7f5225c0271068aa2c61b
Kartik Sharma <kartiksharma9319@gmail.com>
2024-11-14 07:42:47 +0000

    feat: Reliever Assignment doctype to transfer responsibilites

---
+++ one_fm/one_fm/doctype/reliever_assignment/reliever_assignment.js
@@ -0,0 +1,18 @@
+// Copyright (c) 2024, omar jaber and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("Reliever Assignment", {
+	setup (frm) {
+        let today = moment().format("YYYY-MM-DD");
+        frm.set_query("leave_application", () => {
+            return {
+                filters: {
+                    workflow_state: "Approved",
+                    from_date: ["<=", today],
+                    to_date: [">=", today],
+                    custom_reliever_ : ["is", "set"]
+                }
+            }
+        }) 
+	},
+});

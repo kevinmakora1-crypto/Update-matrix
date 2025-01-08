6154bbfde49e0ea2f68d1a712c2db70c928b00b2
samdanikouser <samdani.shareef6@gmail.com>
2025-01-08 14:01:32 +0300

    coding efficiently

--- one_fm/patches/v15_0/update_employees_operation_role.py
+++ one_fm/patches/v15_0/update_employees_operation_role.py
@@ -7,26 +7,24 @@ def execute():
     if not employees_name:
         return []
     query = """
-        SELECT *
-        FROM `tabEmployee Schedule`
-        WHERE roster_type = 'Basic'
-        AND employee_availability = 'Working'
-        AND employee IN %(employee_names)s
-        AND date = (
+        SELECT es.employee,es.shift,es.operations_role
+        FROM `tabEmployee Schedule` as es
+        WHERE es.roster_type = 'Basic'
+        AND es.employee_availability = 'Working'
+        AND es.employee IN %(employee_names)s
+        AND es.date = (
             SELECT MAX(date)
             FROM `tabEmployee Schedule` AS sub
-            WHERE sub.employee = `tabEmployee Schedule`.employee
+            WHERE sub.employee = es.employee
         )
     """
     schedules = frappe.db.sql(query, {"employee_names": employees_name}, as_dict=True)
+    schedule_map = {schedule['employee']: schedule for schedule in schedules}
 
-    combined_data = [{
-        'employee':employee_name,
-        'schedule':next((schedule for schedule in schedules if schedule.employee==employee_name), None)
-    } for employee_name in employees_name]
-
-    for employee in combined_data:
-        update_operation_role = frappe.get_doc('Employee',employee['employee'])
-        update_operation_role.custom_operations_role_allocation = employee['schedule']['operations_role']
-        update_operation_role.save(ignore_permissions=True)
-        frappe.db.commit()
+    for employee in employees_name:
+        update_operation_role = frappe.get_doc('Employee',employee)
+        schedule = schedule_map.get(employee)
+        if schedule and update_operation_role.shift and update_operation_role.shift == schedule['shift']:
+            update_operation_role.custom_operations_role_allocation = schedule['operations_role']
+            update_operation_role.save(ignore_permissions=True)
+    frappe.db.commit()

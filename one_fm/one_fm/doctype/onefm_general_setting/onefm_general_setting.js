df434682327bd384ca69e8ce1645d5541bbb9b6f
Muhammad Ibtesam Arshad <m.arshad@one-fm.com>
2025-01-21 19:14:46 +0500

    removed sync google tasks button from onefm general settings

--- one_fm/one_fm/doctype/onefm_general_setting/onefm_general_setting.js
+++ one_fm/one_fm/doctype/onefm_general_setting/onefm_general_setting.js
@@ -5,22 +5,12 @@ frappe.ui.form.on('ONEFM General Setting', {
 	refresh: function(frm) {
 		if(!frm.is_new()){
 			frm.trigger('setup_face_recognition');
-			frm.trigger('sync_google_tasks_with_erp_todos');
 		}
 	},
 	setup_face_recognition(frm){
 		frm.add_custom_button('Setup Face Recognition', ()=>{
 			frappe.call({
 				method: 'one_fm.api.utils.set_up_face_recognition_server_credentials'
-			}).then(res=>{
-				frappe.msgprint(res.message.message)
-			})
-		}, 'Actions')
-	},
-	sync_google_tasks_with_erp_todos(frm){
-		frm.add_custom_button('Sync Google Tasks', ()=>{
-			frappe.call({
-				method: 'one_fm.overrides.todo.sync_google_tasks_with_todos'
 			}).then(res=>{
 				frappe.msgprint(res.message.message)
 			})

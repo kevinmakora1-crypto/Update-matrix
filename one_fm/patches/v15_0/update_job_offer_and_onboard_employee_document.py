fa068e396ce68d2dd534336d577b4c315c658b84
NoBoneZ <adekunleabraham09@gmail.com>
2025-02-06 12:23:13 +0100

    added job offer

--- one_fm/patches/v15_0/update_job_offer_and_onboard_employee_document.py
+++ one_fm/patches/v15_0/update_job_offer_and_onboard_employee_document.py
@@ -2,7 +2,7 @@ import frappe
 
 
 def execute():
-    onboard_employee_names = ["EMP-ONB-2022-00303"]
+    onboard_employee_names = ["EMP-ONB-2025-00035", "EMP-ONB-2025-00032"]
 
     job_offer_names = frappe.get_all(
         "Onboard Employee",
@@ -10,17 +10,19 @@ def execute():
         pluck="job_offer"
     )
 
+    job_offer_names.extend(["HR-OFF-2025-00202", "HR-OFF-2025-00201"])
+
     job_offer_names = tuple(job_offer_names) if job_offer_names else ("",)
 
     frappe.db.sql("""
         UPDATE `tabOnboard Employee`
-        SET provide_accommodation_by_company = 0, provide_transportation_by_company = 0
+        SET provide_accommodation_by_company = 1, provide_transportation_by_company = 1
         WHERE name IN %(names)s
     """, {"names": tuple(onboard_employee_names)})
 
     if job_offer_names != ("",):
         frappe.db.sql("""
             UPDATE `tabJob Offer`
-            SET one_fm_provide_accommodation_by_company = 0, one_fm_provide_transportation_by_company = 0
+            SET one_fm_provide_accommodation_by_company = 1, one_fm_provide_transportation_by_company = 1
             WHERE name IN %(names)s
         """, {"names": job_offer_names})

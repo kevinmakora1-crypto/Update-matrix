3a409c44d8154f590232a49f81683c0e9fe8d35a
samdanikouser <samdani.shareef6@gmail.com>
2025-02-11 15:35:42 +0300

    leave status for annual leave

--- one_fm/api/v1/leave_application.py
+++ one_fm/api/v1/leave_application.py
@@ -344,7 +344,7 @@ def new_leave_application(employee: str, from_date: str,to_date: str,leave_type:
         _file = upload_file(leave, "", attachments['attachment_hashed_name'], "", attachments['attachment_file'], is_private=True)
         leave.append('proof_documents', {'description':attachments['attachment_name'], 
             "attachments":_file.file_url})
-        leave.save()
+    leave.save()
     # add the files to File doctype
     return leave.as_dict()
 

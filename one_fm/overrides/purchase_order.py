696938a6580deb6c53e5a53d7a2bd7c69b12bb8b
Ebuka <ebukaakeru@gmail.com>
2025-03-02 22:08:56 +0100

    set PO approval dates

--- one_fm/overrides/purchase_order.py
+++ one_fm/overrides/purchase_order.py
@@ -1,6 +1,43 @@
 import frappe
 from frappe import _
+from frappe.utils import nowdate
 
+
+
+def set_workflow_states(doc,old_doc):
+    """Sets approval dates based on workflow state changes."""
+    current_workflow_state = doc.workflow_state
+
+    # If the workflow state didn't change, no action is needed
+    if old_doc.workflow_state == current_workflow_state:
+        return
+
+    # Mapping workflow states to custom field names
+    workflow_mapping = {
+        "Pending Purchase Manager": "custom_purchase_officer_approval_date",
+        "Pending Finance Approver": "custom_purchase_manager_approval_date",
+    }
+
+    # Set approval dates based on current workflow state
+    if current_workflow_state in workflow_mapping:
+        doc.db_set(workflow_mapping[current_workflow_state],nowdate())
+
+
+
+def on_update(doc, event):
+    """Handles the update event for the Purchase Order document."""
+    #Check if workflow_state was changed, if it wasn't then we return,
+    old_doc = doc.get_doc_before_save()
+    if not old_doc:
+        return
+    if old_doc.workflow_state == doc.workflow_state:
+        return
+    # Check if both dates are present; if so, no action is needed.
+    if doc.custom_purchase_officer_approval_date and doc.custom_purchase_manager_approval_date:
+        return
+    set_workflow_states(doc,old_doc)
+    
+    
 def validate_purchase_uom(doc, method):
     for item in doc.items:
         query = """

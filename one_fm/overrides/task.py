463ca27cde6e8dff673572a1e408b45446315c82
yusuffAFEX <yusufoyedele43@gmail.com>
2025-04-22 08:08:42 +0100

    189048088

--- one_fm/overrides/task.py
+++ one_fm/overrides/task.py
@@ -31,8 +31,8 @@ def validate_task(doc, method):
                     pluck="name"
                 )
 
-                for todo_name in todos:
-                    frappe.db.set_value("ToDo", todo_name, "status", "Closed")
+                if todos: 
+                    frappe.db.set_value("ToDo", {"name": ["in", todos]}, "status", "Closed")
 
     roles = get_user_roles()
     is_manager = is_project_manager(doc.project) if doc.project else False

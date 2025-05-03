5626b5d55b634dfc74b97df05300dd42c07e98a1
samdanikouser <samdani.shareef6@gmail.com>
2025-05-03 13:52:19 +0300

    employee monthly action

---
+++ one_fm/one_fm/doctype/employee_monthly_action/employee_monthly_action.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2025, omar jaber and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+
+class EmployeeMonthlyAction(Document):
+    def on_submit(self):
+        
+        for row in self.goal_update:
+            goal_doc = frappe.get_value("Goal", {"goal_name": row.goal}, "name")
+            if goal_doc:
+                frappe.db.set_value("Goal", goal_doc, "progress", row.current_progress)

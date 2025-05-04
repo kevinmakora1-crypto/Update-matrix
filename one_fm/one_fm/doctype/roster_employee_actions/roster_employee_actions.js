9183a8b6ed1ac0269003100dfb579049060e4182
samdanikouser <samdani.shareef6@gmail.com>
2025-05-04 22:44:23 +0300

    take action chnages

--- one_fm/one_fm/doctype/roster_employee_actions/roster_employee_actions.js
+++ one_fm/one_fm/doctype/roster_employee_actions/roster_employee_actions.js
@@ -1,39 +1,34 @@
-// Copyright (c) 2021, omar jaber and contributors
-// For license information, please see license.txt
 frappe.ui.form.on('Roster Employee Actions', {
-	onload(frm) {
-		render_buttons_in_child(frm);
-	}
+    refresh(frm) {
+        frm.fields_dict["employees_not_rostered"].grid.grid_rows.forEach(row => {
+            add_action_button(row, frm);
+        });
+
+        // Also handle dynamically rendered rows
+        frm.fields_dict["employees_not_rostered"].grid.on('grid-row-render', function (grid_row) {
+            add_action_button(grid_row, frm);
+        });
+    }
 });
 
-function render_buttons_in_child(frm) {
-    (frm.doc.employees_not_rostered || []).forEach((row, i) => {
-        const button_html = `<button class="btn btn-xs btn-primary" onclick="takeAction('${row.name}')">Take Action</button>`;
-        frappe.model.set_value(row.doctype, row.name, 'take_action', button_html);
-    });
-}
-window.takeAction = function(rowname) {
-    const row = frappe.get_doc(cur_frm.doc.doctype, cur_frm.doc.name)
-        .employees_not_rostered.find(r => r.name === rowname);
+function add_action_button(grid_row, frm) {
+    const row = grid_row.doc;
 
-    if (!row || !row.employee) {
-        frappe.msgprint("No employee data found.");
-        return;
-    }
+    const $cell = $(grid_row.row).find('[data-fieldname="take_action"]');
+    $cell.empty();
 
-    frappe.db.get_doc('Employee', row.employee).then(employee => {
-        const url = `/app/roster?main_view=roster&sub_view=basic&roster_type=basic&employee_id=${employee.employee_id}&shift=${row.shift_allocation}`;
-        window.open(url, '_blank');
-    });
-}
-
-frappe.ui.form.on('Employees Not Rostered', {
-    take_action(frm, cdt, cdn) {
-        let row = locals[cdt][cdn];
+    const $btn = $(`<button class="btn btn-xs btn-primary">Take Action</button>`);
+    $btn.on('click', () => {
+        if (!row.employee) {
+            frappe.msgprint("No employee selected.");
+            return;
+        }
 
         frappe.db.get_doc('Employee', row.employee).then(employee => {
             const url = `/app/roster?main_view=roster&sub_view=basic&roster_type=basic&employee_id=${employee.employee_id}&shift=${row.shift_allocation}`;
             window.open(url, '_blank');
         });
-    }
-});
+    });
+
+    $cell.append($btn);
+}

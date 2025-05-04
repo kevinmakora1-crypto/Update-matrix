76507a0cf8f4a283857c7ffdb6bf82a7ca9f94ac
zeinaremah <zeinaremah@gmail.com>
2025-05-04 15:23:51 +0300

    added custom readonly datetime field to capture application date in job applicant doctype + set field at time of record creation

---
+++ one_fm/setup/setup.py
@@ -0,0 +1,22 @@
+from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
+
+def after_install():
+    create_custom_fields(get_custom_fields())
+
+def get_custom_fields():
+    custom_fields = {}
+    custom_fields.add(get_job_applicant_custom_fields())
+    return custom_fields
+
+def get_job_applicant_custom_fields():
+    return {
+        "Job Applicant": [
+            {
+                "fieldname": "application_date",
+                "fieldtype": "Datetime",
+                "label": "Application Date",
+                "insert_after": "country",
+                "read_only": 1,
+            },
+        ]
+    }

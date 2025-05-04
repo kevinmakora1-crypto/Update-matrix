199f47da70acfccb85211b19675334b7538290ac
NoBoneZ <adekunleabraham09@gmail.com>
2025-05-04 09:08:40 +0100

    added blockers and test

--- one_fm/one_fm/doctype/employee_weekly_action/employee_weekly_action.py
+++ one_fm/one_fm/doctype/employee_weekly_action/employee_weekly_action.py
@@ -4,12 +4,39 @@ from datetime import date, timedelta
 
 import frappe
 from frappe.model.document import Document
+from frappe.utils import get_datetime
 
 from one_fm.api.v1.utils import response
 
 
 class EmployeeWeeklyAction(Document):
-	pass
+	
+
+	def on_submit(self):
+		self.create_blockers()
+
+	def create_blockers(self):
+		if not self.blockers:
+			return
+
+		for blocker in self.blockers:
+			doc = frappe.new_doc("Blocker")
+			doc.user = frappe.session.user
+			doc.date = get_datetime().date()
+			doc.assigned_to = self.get_reporting_manager()
+			doc.priority = blocker.priority
+			doc.blocker_details = blocker.problem
+			doc.insert(ignore_permissions=True)
+
+		frappe.db.commit()
+
+
+	def get_reporting_manager(self):
+		employee_id = frappe.db.get_value("Employee", {"user_id": frappe.session.user}, "reports_to")
+		return frappe.db.get_value("Employee", employee_id, "user_id") if employee_id else None
+
+			
+	
 
 
 def get_week_dates(offset=0):

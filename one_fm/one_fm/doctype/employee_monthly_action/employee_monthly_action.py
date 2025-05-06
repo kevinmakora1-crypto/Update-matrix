70244a7e8afe6ab0e6499b2b1312399433b0562a
samdanikouser <samdani.shareef6@gmail.com>
2025-05-06 12:46:49 +0300

    check to updated non 0 values

--- one_fm/one_fm/doctype/employee_monthly_action/employee_monthly_action.py
+++ one_fm/one_fm/doctype/employee_monthly_action/employee_monthly_action.py
@@ -9,5 +9,5 @@ class EmployeeMonthlyAction(Document):
     def on_submit(self):
         for row in self.goal_update:
             goal_doc = frappe.get_value("Goal", {"goal_name": row.goal}, "name")
-            if goal_doc:
+            if goal_doc and row.current_progress:
                 frappe.db.set_value("Goal", goal_doc, "progress", row.current_progress)

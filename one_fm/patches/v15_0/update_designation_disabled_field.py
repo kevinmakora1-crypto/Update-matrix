4f74cd4d7cdf005fcddb1b43c809c9d2b1cd0c1f
NoBoneZ <adekunleabraham09@gmail.com>
2025-05-11 12:55:33 +0100

    removed property

--- one_fm/patches/v15_0/update_designation_disabled_field.py
+++ one_fm/patches/v15_0/update_designation_disabled_field.py
@@ -8,7 +8,6 @@ def execute():
 				"fieldtype": "Check",
 				"label": "Disabled",
 				"insert_after": "custom_job_offer_term_template",
-				"in_list_view": 1,
 				"description": "This field is used to disable a designation, ensuring it does not appear in the system when filtering or selecting designations."
 			},
 		]

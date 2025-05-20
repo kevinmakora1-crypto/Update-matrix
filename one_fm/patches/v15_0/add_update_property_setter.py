e730d00f135a96f698240bdcb95ccde6f71bc4c7
Jamsheer PP <kuttypjamsheer@gmail.com>
2025-05-20 15:13:43 +0530

    Revert "feat: custom fixtures"

--- one_fm/patches/v15_0/add_update_property_setter.py
+++
@@ -1,6 +0,0 @@
-import frappe
-from one_fm.custom.property_setter.property_setter import create_property_setter
-from one_fm.setup import get_field_properties
-
-def execute():
-    create_property_setter(get_field_properties())

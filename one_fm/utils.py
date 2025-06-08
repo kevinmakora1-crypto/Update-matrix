e9ab3a614d38fc39cb778a8b9508c271a18a0550
abraham_adekunle <gadekunle@afexnigeria.com>
2025-06-08 14:29:25 +0100

    exported customization and added logic check

--- one_fm/utils.py
+++ one_fm/utils.py
@@ -3706,6 +3706,7 @@ def set_employee_status():
             'status': 'Approved',
             'from_date': ['<=', current_date],
             'to_date': ['>=', add_days(current_date, -1)],
+            "leave_type": ["IN", fetch_leave_types_update_employee_status()]
         },
         fields=['employee', 'employee.status', 'from_date', 'to_date', 'custom_reliever_', 'name']  # Fetch employee status directly
     )
@@ -3985,5 +3986,7 @@ def update_fields_in_doctypes(data):
 					doc.save()
 
 
+def fetch_leave_types_update_employee_status():
+    return set(frappe.db.get_list("Leave Type", {"custom_update_employee_status_to_vacation": True}, pluck="name"))
 
    

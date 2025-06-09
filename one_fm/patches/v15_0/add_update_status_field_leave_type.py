6d792745dc443a44fef0858eec18ddfe3da7a7fa
NoBoneZ <adekunleabraham09@gmail.com>
2025-06-09 14:30:41 +0100

    used update_custom field patch

--- one_fm/patches/v15_0/add_update_status_field_leave_type.py
+++
@@ -1,7 +0,0 @@
-from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
-
-from one_fm.one_fm.custom.custom_field.leave_type import get_leave_type_custom_fields
-
-
-def execute():
-	create_custom_fields(get_leave_type_custom_fields())

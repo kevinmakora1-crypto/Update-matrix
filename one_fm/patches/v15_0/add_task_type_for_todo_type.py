5f76a5f8d29293573bc658372414c8f0c3b56606
Jamsheer PP <kuttypjamsheer@gmail.com>
2025-07-02 12:56:30 +0530

    fix: task type patch

--- one_fm/patches/v15_0/add_task_type_for_todo_type.py
+++ one_fm/patches/v15_0/add_task_type_for_todo_type.py
@@ -3,6 +3,7 @@ import frappe
 def execute():
     task_types = ["Routine", "Action", "Project"]
     for task_type in task_types:
-        doc = frappe.new_doc("Task Type")
-        doc.__newname = task_type
-        doc.insert(ignore_permissions=True)
+        if not frappe.db.exists("Task Type", task_type):
+            doc = frappe.new_doc("Task Type")
+            doc.__newname = task_type
+            doc.insert(ignore_permissions=True)

02a258e708f0c44e31bac61649dbb8c192edafea
Muhammad Ibtesam Arshad <m.arshad@one-fm.com>
2025-07-07 12:19:03 +0500

    feat: added weekend reliever in staff roster

--- one_fm/one_fm/page/roster/roster.py
+++ one_fm/one_fm/page/roster/roster.py
@@ -56,7 +56,7 @@ def get_staff(assigned=1, employee_id=None, employee_name=None, company=None, pr
         select
             distinct emp.name, emp.employee_id, emp.employee_name, emp.image, emp.one_fm_nationality as nationality,
            usr.mobile_no, usr.name as email, emp.designation, emp.department, emp.shift, emp.site,
-         emp.project,opsite.account_supervisor_name as site_supervisor,opshift.supervisor_name as shift_supervisor, emp.custom_operations_role_allocation, emp.custom_is_reliever
+         emp.project,opsite.account_supervisor_name as site_supervisor,opshift.supervisor_name as shift_supervisor, emp.custom_operations_role_allocation, emp.custom_is_reliever, emp.custom_is_weekend_reliever
         from `tabEmployee` as emp, `tabUser` as usr,`tabOperations Shift` as opshift,`tabOperations Site` as opsite
         where
         emp.project is not NULL
@@ -1566,7 +1566,7 @@ def get_shift_details_of_employee(emp,date):
 
 
 @frappe.whitelist()
-def assign_staff(employees, shift, custom_is_reliever, custom_operations_role_allocation=None, request_employee_assignment=None):
+def assign_staff(employees, shift, custom_is_reliever, custom_is_weekend_reliever,custom_operations_role_allocation=None, request_employee_assignment=None):
     if not employees:
         frappe.throw("Please select employees first")
     validation_logs = []
@@ -1585,7 +1585,7 @@ def assign_staff(employees, shift, custom_is_reliever, custom_operations_role_al
 
             for employee in json.loads(employees):
                 if not cint(request_employee_assignment):
-                    frappe.enqueue(assign_job, employee=employee, shift=shift, site=site, project=project, custom_operations_role_allocation=custom_operations_role_allocation, custom_is_reliever=custom_is_reliever, is_async=True, queue="long")
+                    frappe.enqueue(assign_job, employee=employee, shift=shift, site=site, project=project, custom_operations_role_allocation=custom_operations_role_allocation, custom_is_reliever=custom_is_reliever, custom_is_weekend_reliever=custom_is_weekend_reliever,is_async=True, queue="long")
                 else:
                     emp_project, emp_site, emp_shift = frappe.db.get_value("Employee", employee, ["project", "site", "shift"])
                     site, project = frappe.get_value("Operations Shift", shift, ["site", "project"])
@@ -1607,13 +1607,14 @@ def create_request_employee_assignment(employee, from_shift, to_shift):
     req_ea_doc.save(ignore_permissions=True)
 
 
-def assign_job(employee, shift, site, project, custom_operations_role_allocation, custom_is_reliever):
+def assign_job(employee, shift, site, project, custom_operations_role_allocation, custom_is_reliever, custom_is_weekend_reliever):
 
     frappe.set_value("Employee", employee, "shift", shift)
     frappe.set_value("Employee", employee, "site", site)
     frappe.set_value("Employee", employee, "project", project)
     frappe.set_value("Employee", employee, "custom_operations_role_allocation", custom_operations_role_allocation)
     frappe.set_value("Employee", employee, "custom_is_reliever", custom_is_reliever)
+    frappe.set_value("Employee", employee, "custom_is_weekend_reliever", custom_is_weekend_reliever)
 
 @frappe.whitelist(allow_guest=True)
 def search_staff(key, search_term):
@@ -1797,6 +1798,7 @@ def get_employee_details(employee_id):
         "site": employee.site,
         "shift": employee.shift,
         "custom_is_reliever": employee.custom_is_reliever,
+        "custom_is_weekend_reliever": employee.custom_is_weekend_reliever,
         "custom_operations_role_allocation": employee.custom_operations_role_allocation
     }
 

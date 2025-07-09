8ab7d1291effb33dc7cd63f5d6999a6565500c49
Muhammad Ibtesam Arshad <m.arshad@one-fm.com>
2025-07-09 12:52:28 +0500

    removed console

--- one_fm/one_fm/doctype/roster_employee_actions/roster_employee_actions.js
+++ one_fm/one_fm/doctype/roster_employee_actions/roster_employee_actions.js
@@ -1,7 +1,6 @@
 frappe.ui.form.on('Roster Employee Actions', {
     refresh(frm) {
         frm.add_custom_button(__('Take Action'), function() {
-            console.log(frm.doc)
             const url = `/app/roster?main_view=roster&sub_view=basic&roster_type=basic&employee_id=${frm.doc.employee_id}&shift=${frm.doc.shift}`;
             window.open(url, '_blank');
         });

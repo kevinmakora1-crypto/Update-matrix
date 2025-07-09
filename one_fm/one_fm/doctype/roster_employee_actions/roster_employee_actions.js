6319651130731dbe92dfd308420d2be4159b91eb
Muhammad Ibtesam Arshad <m.arshad@one-fm.com>
2025-07-09 18:11:47 +0500

    updated query params for roster

--- one_fm/one_fm/doctype/roster_employee_actions/roster_employee_actions.js
+++ one_fm/one_fm/doctype/roster_employee_actions/roster_employee_actions.js
@@ -1,7 +1,10 @@
 frappe.ui.form.on('Roster Employee Actions', {
     refresh(frm) {
         frm.add_custom_button(__('Take Action'), function() {
-            const url = `/app/roster?main_view=roster&sub_view=basic&roster_type=basic&employee_id=${frm.doc.employee_id}&shift=${frm.doc.shift}`;
+            const employeeID = encodeURIComponent(frm.doc.employee_id || '');
+            const shift = encodeURIComponent(frm.doc.shift || '');
+
+            const url = `/app/roster?main_view=roster&sub_view=basic&roster_type=basic&employee_id=${employeeID}&shift=${shift}`;
             window.open(url, '_blank');
         });
     }

d9e994a55be9878329c19e7c84209e6b247a9b7b
Jamsheer PP <kuttypjamsheer@gmail.com>
2025-07-17 01:13:29 +0530

    chore: assignment rule - shift permission

--- one_fm/patches/v15_0/add_update_assignment_rule.py
+++ one_fm/patches/v15_0/add_update_assignment_rule.py
@@ -4,3 +4,4 @@ from one_fm.custom.assignment_rule.assignment_rule import get_assignment_rule_js
 def execute():
     create_assignment_rule(get_assignment_rule_json_file("roster_post_action_site_supervisor.json"))
     create_assignment_rule(get_assignment_rule_json_file("subcontract_staff_shortlist.json"))
+    create_assignment_rule(get_assignment_rule_json_file("shift_permission_approver.json"))

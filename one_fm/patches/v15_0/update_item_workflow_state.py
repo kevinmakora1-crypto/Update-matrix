97a8c24337bceb44c5125ac961e48c48151f06fa
Ebuka <ebukaakeru@gmail.com>
2025-10-16 12:40:33 +0100

    set all approved items in Approved Workflow state

---
+++ one_fm/patches/v15_0/update_item_workflow_state.py
@@ -0,0 +1,8 @@
+import frappe
+
+def execute():
+    """Update Item Workflow State For items which has disabled checkbox unchecked"""
+    items = frappe.get_all("Item", filters={"disabled": 0,'workflow_state':['!=',"Approved"]}, pluck="name")
+    for item in items:
+        frappe.db.set_value("Item", item, "workflow_state", "Approved")
+    frappe.db.commit()

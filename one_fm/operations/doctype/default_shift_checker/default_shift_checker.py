dba5bdba6936acf13bb242fb25d963f66be3c6ba
Muhammad Ibtesam Arshad <m.arshad@one-fm.com>
2025-10-19 15:41:45 +0500

    added dynamic comment for default shift checker

--- one_fm/operations/doctype/default_shift_checker/default_shift_checker.py
+++ one_fm/operations/doctype/default_shift_checker/default_shift_checker.py
@@ -158,8 +158,12 @@ def create_checker(start_date, end_date, is_day_off_reliever=False, is_weekend_r
 
 			if is_day_off_reliever:
 				doc.is_day_off_reliever = 1
+				doc.comment = f"Day Off Reliever has been assigned to the same shift for up to {threshold} time(s). Either allocate the Employee to the Shift and unmark as Day Off Reliever or reduce his/her schedule to the shift to less than {threshold} time(s)."
 			if is_weekend_reliever:
 				doc.is_weekend_reliever = 1
+				doc.comment = f"Weekend Reliever has been assigned to the same shift for up to {threshold} time(s). Either allocate the Employee to the Shift and unmark as Weekend Reliever or reduce his/her schedule to the shift to less than {threshold} time(s)."
+			else:
+				doc.comment = f"Employee is not set as a Reliever and has been Rostered in other shifts for at least {threshold} time(s). Either reduce the allocation in each of the Shifts below the {threshold} or set the Employee as a reliever."
 
 			# Determine shift condition
 			shift_condition = (EmployeeSchedule.shift == employee.default_shift) if is_reliever else (EmployeeSchedule.shift != employee.default_shift)

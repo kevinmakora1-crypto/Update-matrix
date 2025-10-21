34fd2e318f87996997a007d5e171b4b399bb42b4
Muhammad Ibtesam Arshad <m.arshad@one-fm.com>
2025-10-21 15:43:33 +0500

    updated disabling logic for task fields

--- one_fm/public/js/doctype_js/task.js
+++ one_fm/public/js/doctype_js/task.js
@@ -24,7 +24,7 @@ function set_perms(frm) {
 
                 // Lock entire form.
                 if (!roles.includes("Projects Manager") && !roles.includes("Projects User") && !is_project_manager && frm.doc.owner !== frappe.session.user) {
-                    (frm.fields || []).forEach(field => frm.set_df_property(field.fieldname, "read_only", 1));
+                    Object.keys(frm.fields_dict || {}).forEach(f => frm.set_df_property(f, "read_only", 1));
                     return;
                 }
 

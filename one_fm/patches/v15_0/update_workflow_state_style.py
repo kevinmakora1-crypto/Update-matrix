e6f07d2753ce10323c6901c8bc42fa2cd605e8e1
NoBoneZ <adekunleabraham09@gmail.com>
2025-10-29 11:33:02 +0100

    updated property of field in workflow state

---
+++ one_fm/patches/v15_0/update_workflow_state_style.py
@@ -0,0 +1,15 @@
+import frappe
+
+def execute():	
+	if not frappe.db.exists("Property Setter", "Workflow State-style-reqd"):
+		frappe.get_doc({
+			"doctype": "Property Setter",
+			"doctype_or_field": "DocField",
+			"doc_type": "Workflow State",
+			"field_name": "style",
+			"property": "reqd",
+			"property_type": "Check",
+			"value": "1"
+		}).insert(ignore_permissions=True)
+		
+		frappe.db.commit()

59e87bf5628b906037c2a53b5ad18ae0c3ada772
NoBoneZ <adekunleabraham09@gmail.com>
2025-10-30 12:01:43 +0100

    added workflow and assignment rule

---
+++ one_fm/patches/v15_0/update_fingerprint_appointment_workflow.py
@@ -0,0 +1,12 @@
+import frappe
+from one_fm.custom.workflow.workflow import delete_workflow, get_workflow_json_file, create_workflow
+from one_fm.setup.assignment_rule import create_assignment_rule, get_assignment_rule_json_file
+
+def execute():
+    delete_workflow(get_workflow_json_file("fingerprint_appointment.json"))
+    create_workflow(get_workflow_json_file("fingerprint_appointment.json"))
+
+    create_assignment_rule(get_assignment_rule_json_file("action_fingerprint_appointment_pro.json"))
+    create_assignment_rule(get_assignment_rule_json_file("action_fingerprint_appointment_supervisor.json"))
+    create_assignment_rule(get_assignment_rule_json_file("action_fingerprint_appointment_transportation_supervisor.json"))
+    

8638ad298345b59e944b6eb4198badcb41c5e9fd
jam <jam@jams-MacBook-Air.local>
2025-11-07 22:20:51 +0530

    feat: medical appointment - set supervisor

--- one_fm/grd/doctype/medical_appointment/medical_appointment.js
+++ one_fm/grd/doctype/medical_appointment/medical_appointment.js
@@ -2,6 +2,21 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Medical Appointment", {
-	// refresh(frm) {
-	// },
+	employee(frm) {
+		frm.events.fetch_approver(frm);
+	},
+	fetch_approver(frm) {
+		frm.set_value("employee_supervisor", null);
+		if(frm.doc.employee){
+			frappe.call({
+				method: "one_fm.utils.get_approver_user",
+				args : {"employee": frm.doc.employee},
+				callback: function (r) {
+					if(r && r.message){
+						frm.set_value("employee_supervisor", r.message);
+					}
+				}
+			});
+		}
+	}
 });
--- one_fm/grd/doctype/medical_appointment/medical_appointment.py
+++ one_fm/grd/doctype/medical_appointment/medical_appointment.py
@@ -5,7 +5,7 @@ import frappe
 from frappe import _
 from frappe.model.document import Document
 from frappe.utils import today
-
+from one_fm.utils import get_approver_user
 
 class MedicalAppointment(Document):
 	def validate(self):
@@ -15,3 +15,8 @@ class MedicalAppointment(Document):
 					_("You are not allowed to action medical appointment before the appointment date."),
 					title=_("Appointment Date is in the future"),
 				)
+		self.set_approver()
+
+	def set_approver(self):
+		if not self.employee_supervisor and self.employee:
+			self.employee_supervisor = get_approver_user(self.employee)

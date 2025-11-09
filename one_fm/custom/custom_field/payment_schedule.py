76466cf3decfc915b8debf83f4dc3367dbc93920
samdanikouser <samdani.shareef6@gmail.com>
2025-11-09 16:19:03 +0300

    testing for daytona

--- one_fm/custom/custom_field/payment_schedule.py
+++ one_fm/custom/custom_field/payment_schedule.py
@@ -2,13 +2,6 @@ def get_payment_schedule_custom_fields():
     return {
         "Payment Schedule": [
             {
-                "fieldname": "credit_days",
-                "fieldtype": "Int",
-                "insert_after": "due_date",
-                "label": "Credit Days",
-                "bold": 1
-            },
-            {
                 "fieldname": "one_fm_payment",
                 "fieldtype": "Select",
                 "insert_after": "mode_of_payment",

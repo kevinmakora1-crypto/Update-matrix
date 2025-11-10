c734c1dac832946b358878bad094eeaf3872d3cb
jam <jam@jams-MacBook-Air.local>
2025-11-10 18:12:21 +0530

    feat: Notify the GR Operator on supervisor rejection

--- one_fm/grd/doctype/medical_appointment/medical_appointment.js
+++ one_fm/grd/doctype/medical_appointment/medical_appointment.js
@@ -18,5 +18,34 @@ frappe.ui.form.on("Medical Appointment", {
 				}
 			});
 		}
+	},
+	before_workflow_action: async function(frm){
+		let workflow_states = ["Set Pick-up as Accommodation (Supervisor)", "Pending Supervisor"];
+		if (frm.selected_workflow_action == "Reject" && workflow_states.includes(frm.doc.workflow_state) && !frm.doc.reason_for_rejection) {
+			await new Promise((resolve, reject) => {
+                frappe.dom.unfreeze();
+				var dialog = new frappe.ui.Dialog({
+					title: __("Reason for Rejection"),
+					fields: [
+						{
+							label: __("Reason for Rejection"),
+							fieldname: "reason_for_rejection",
+							fieldtype: "Select",
+							options: "\nStaff Shortage\nAnother Employee has appointment from same site\nClient requested another appointment",
+							reqd: 1
+						}
+					],
+					primary_action_label: __("Confirm"),
+					primary_action(values) {
+						frm.set_value("reason_for_rejection", values.reason_for_rejection);
+						dialog.hide();
+						frm.save()
+                            .then(resolve)
+                            .catch(reject);
+					}
+				});
+				dialog.show();
+			});
+		}
 	}
 });

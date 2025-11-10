8c24d19a2f0b38e1b4cf1b448e12dc569041198e
jam <jam@jams-MacBook-Air.local>
2025-11-10 18:48:58 +0530

    feat: validate payment invoice attach

--- one_fm/grd/doctype/medical_appointment/medical_appointment.js
+++ one_fm/grd/doctype/medical_appointment/medical_appointment.js
@@ -19,33 +19,46 @@ frappe.ui.form.on("Medical Appointment", {
 			});
 		}
 	},
-	before_workflow_action: async function(frm){
+	before_workflow_action(frm) {
+		frm.events.validate_payment_invoice(frm);
+		return new Promise((resolve, reject) => {
+			frappe.dom.unfreeze();
+			frm.events.capture_rejection_reason(frm, resolve, reject);
+		});
+	},
+	capture_rejection_reason(frm, resolve, reject){
 		let workflow_states = ["Set Pick-up as Accommodation (Supervisor)", "Pending Supervisor"];
 		if (frm.selected_workflow_action == "Reject" && workflow_states.includes(frm.doc.workflow_state) && !frm.doc.reason_for_rejection) {
-			await new Promise((resolve, reject) => {
-                frappe.dom.unfreeze();
-				var dialog = new frappe.ui.Dialog({
-					title: __("Reason for Rejection"),
-					fields: [
-						{
-							label: __("Reason for Rejection"),
-							fieldname: "reason_for_rejection",
-							fieldtype: "Select",
-							options: "\nStaff Shortage\nAnother Employee has appointment from same site\nClient requested another appointment",
-							reqd: 1
-						}
-					],
-					primary_action_label: __("Confirm"),
-					primary_action(values) {
-						frm.set_value("reason_for_rejection", values.reason_for_rejection);
-						dialog.hide();
-						frm.save()
-                            .then(resolve)
-                            .catch(reject);
+			var dialog = new frappe.ui.Dialog({
+				title: __("Reason for Rejection"),
+				fields: [
+					{
+						label: __("Reason for Rejection"),
+						fieldname: "reason_for_rejection",
+						fieldtype: "Select",
+						options: "\nStaff Shortage\nAnother Employee has appointment from same site\nClient requested another appointment",
+						reqd: 1
 					}
-				});
-				dialog.show();
+				],
+				primary_action_label: __("Confirm"),
+				primary_action(values) {
+					frm.set_value("reason_for_rejection", values.reason_for_rejection);
+					dialog.hide();
+					frm.save()
+						.then(resolve)
+						.catch(reject);
+				}
 			});
+			dialog.show();
+		}
+		else {
+			resolve();
+		}
+	},
+	validate_payment_invoice(frm) {
+		if (frm.selected_workflow_action == "Employee Attended" && !frm.doc.payment_invoice) {
+			frappe.dom.unfreeze();
+			frm.scroll_to_field("payment_invoice");
 		}
 	}
 });

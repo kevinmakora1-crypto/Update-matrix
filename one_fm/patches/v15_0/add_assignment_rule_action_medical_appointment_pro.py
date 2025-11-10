717be5b4324a44cef6434bce383bc25034c86425
jam <jam@jams-MacBook-Air.local>
2025-11-10 12:51:42 +0530

    feat: Medical Appointment - Workflow - Assignment

---
+++ one_fm/patches/v15_0/add_assignment_rule_action_medical_appointment_pro.py
@@ -0,0 +1,6 @@
+
+import frappe
+from one_fm.custom.assignment_rule.assignment_rule import get_assignment_rule_json_file, create_assignment_rule
+
+def execute():
+    create_assignment_rule(get_assignment_rule_json_file("action_medical_appointment_pro.json"))

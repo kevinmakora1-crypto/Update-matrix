31b766ddee6c7b1229eea7ccd397c72f82612bd4
jam <jam@jams-MacBook-Air.local>
2025-11-17 18:21:21 +0530

    refactor: designation instead of operations role

--- one_fm/one_fm/doctype/event_staff/event_staff.py
+++ one_fm/one_fm/doctype/event_staff/event_staff.py
@@ -13,7 +13,7 @@ class EventStaff(Document):
 		self.validate_overlapping_event_staff()
 
 	def validate_overlapping_event_staff(self):
-		if not self.employee or not self.client_event or not self.start_date or not self.end_date or not self.operations_role:
+		if not self.employee or not self.client_event or not self.start_date or not self.end_date or not self.designation:
 			return
 		overlapping = frappe.db.sql(
 			"""
@@ -34,33 +34,33 @@ class EventStaff(Document):
 			as_dict=True
 		)
 		if overlapping:
-			msg = f"{self.employee} already assigned to {self.operations_role} from {overlapping[0]['start_date']} to {overlapping[0]['end_date']}."
+			msg = f"{self.employee} already assigned to {self.designation} from {overlapping[0]['start_date']} to {overlapping[0]['end_date']}."
 			frappe.throw(msg)
 
 	def validate_staffing_requirement(self):
-		if not self.client_event or not self.operations_role:
+		if not self.client_event or not self.designation:
 			return
 		requirements = self.get_event_staffing_requirement()
 		if not requirements:
 			return
 		filters = {
 			"client_event": self.client_event,
-			"operations_role": self.operations_role,
+			"designation": self.designation,
 			"docstatus": ["<", 2]
 		}
 		assigned_count = frappe.db.count("Event Staff", filters)
 		if assigned_count > requirements:
-			frappe.throw(f"The number of assigned staff for Operations Role {self.operations_role} exceeds the required count {requirements}.")
+			frappe.throw(f"The number of assigned staff for Designation {self.designation} exceeds the required count {requirements}.")
 
 	def get_event_staffing_requirement(self):
-		if not self.client_event or not self.operations_role:
+		if not self.client_event or not self.designation:
 			return 0
 		requirement = frappe.db.get_value(
 			"Client Event Staff Requirement",
 			{
 				"parent": self.client_event,
 				"parenttype": "Client Event",
-				"operation_role": self.operations_role
+				"designation": self.designation
 			},
 			"count"
 		)
@@ -91,7 +91,6 @@ class EventStaff(Document):
 					"date": date,
 					"shift": self.operations_shift,
 					"shift_type": self.shift_type,
-					"operations_role": self.operations_role,
 					"employee_availability": 'Working',
 					"roster_type": self.roster_type,
 					"site": self.site,
@@ -107,9 +106,6 @@ class EventStaff(Document):
 			)
 			employee_schedule.save(ignore_permissions=True)
 
-			if not self.shift_type:
-				self.create_shift_assignment_for_employee_schedule(employee_schedule, self.event_location)
-
 	def get_event_start_end_date_time(self, date=None):
 		start_time = frappe.utils.get_datetime(self.start_datetime).time()
 		end_time = frappe.utils.get_datetime(self.end_datetime).time()
@@ -123,25 +119,3 @@ class EventStaff(Document):
 			"start_datetime": start_datetime,
 			"end_datetime": end_datetime
 		}
-
-	def create_shift_assignment_for_employee_schedule(self, employee_schedule, event_location):
-		shift_assignment = frappe.get_doc(
-			{
-				"doctype": "Shift Assignment",
-				"employee": employee_schedule.employee,
-				"employee_name": employee_schedule.employee_name,
-				"start_date": employee_schedule.start_datetime.date(),
-				"end_date": employee_schedule.end_datetime.date(),
-				"shift": employee_schedule.shift,
-				"employee_schedule": employee_schedule.name,
-				"site": employee_schedule.site,
-				"project": employee_schedule.project,
-				"is_event_based_shift": 1,
-				"event_staff": self.name,
-				"start_datetime": employee_schedule.start_datetime,
-				"end_datetime": employee_schedule.end_datetime,
-				"site_location": event_location
-			}
-		)
-		shift_assignment.insert(ignore_permissions=True)
-		shift_assignment.submit()

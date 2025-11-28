d8c14d5410ca7bcb0fb6869180ef77f36d79e699
jam <jam@jams-MacBook-Air.local>
2025-11-28 15:18:04 +0530

    init: client interview

---
+++ one_fm/one_fm/doctype/client_interview_shortlist/client_interview_shortlist.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, ONE FM and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Client Interview Shortlist", {
+// 	refresh(frm) {
+
+// 	},
+// });

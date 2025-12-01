1484f8e5dbed36fd15d4c535e7a76c01cebe3846
Muhammad Ibtesam Arshad <m.arshad@one-fm.com>
2025-12-01 19:05:02 +0500

    updated customizations for quality feedback

--- one_fm/custom/custom_field/quality_feedback.py
+++ one_fm/custom/custom_field/quality_feedback.py
@@ -2,7 +2,7 @@ def get_quality_feedback_custom_fields():
 	return {
 		"Quality Feedback": [
 			{
-				"fieldname": "feedback_schedule",
+				"fieldname": "custom_feedback_schedule",
 				"fieldtype": "Link",
 				"label": "Feedback Schedule",
 				"options": "Feedback Schedule",
@@ -12,7 +12,7 @@ def get_quality_feedback_custom_fields():
 				"fetch_from": "template.custom_feedback_schedule"
 			},
 			{
-				"fieldname": "feedback_schedule_stage",
+				"fieldname": "custom_feedback_schedule_stage",
 				"fieldtype": "Link",
 				"label": "Feedback Schedule Stage",
 				"options": "Feedback Schedule Item",
@@ -20,7 +20,7 @@ def get_quality_feedback_custom_fields():
 				"insert_after": "custom_feedback_schedule"
 			},
 			{
-				"fieldname": "item_type",
+				"fieldname": "custom_item_type",
 				"fieldtype": "Link",
 				"label": "Item Type",
 				"options": "Item Type",
@@ -28,7 +28,7 @@ def get_quality_feedback_custom_fields():
 				"insert_after": "custom_feedback_schedule_stage"
 			},
 			{
-				"fieldname": "version_no",
+				"fieldname": "custom_version_no",
 				"fieldtype": "Data",
 				"label": "Version No",
 				"translatable": 1,
@@ -37,7 +37,7 @@ def get_quality_feedback_custom_fields():
 				"fetch_from": "template.custom_version_no"
 			},
 			{
-				"fieldname": "issued_on",
+				"fieldname": "custom_issued_on",
 				"fieldtype": "Date",
 				"label": "Issued On",
 				"read_only": 1,
@@ -45,21 +45,21 @@ def get_quality_feedback_custom_fields():
 				"insert_after": "custom_version_no"
 			},
 			{
-				"fieldname": "feedback_due_on",
+				"fieldname": "custom_feedback_due_on",
 				"fieldtype": "Date",
 				"label": "Feedback Due On",
 				"read_only": 1,
 				"insert_after": "custom_issued_on"
 			},
 			{
-				"fieldname": "employee",
+				"fieldname": "custom_employee",
 				"fieldtype": "Link",
 				"label": "Employee",
 				"options": "Employee",
 				"insert_after": "document_name"
 			},
 			{
-				"fieldname": "employee_name",
+				"fieldname": "custom_employee_name",
 				"fieldtype": "Data",
 				"label": "Employee Name",
 				"read_only": 1,
@@ -68,7 +68,7 @@ def get_quality_feedback_custom_fields():
 				"insert_after": "custom_employee"
 			},
 			{
-				"fieldname": "operations_site",
+				"fieldname": "custom_operations_site",
 				"fieldtype": "Link",
 				"label": "Operations Site",
 				"options": "Operations Site",
@@ -76,7 +76,7 @@ def get_quality_feedback_custom_fields():
 				"insert_after": "custom_employee_name"
 			},
 			{
-				"fieldname": "noticed_damage",
+				"fieldname": "custom_noticed_damage",
 				"fieldtype": "Select",
 				"label": "Noticed Damage?",
 				"options": "\nYes\nNo",
@@ -84,7 +84,7 @@ def get_quality_feedback_custom_fields():
 				"insert_after": "parameters"
 			},
 			{
-				"fieldname": "damage_description",
+				"fieldname": "custom_damage_description",
 				"fieldtype": "Small Text",
 				"label": "Damage Description",
 				"translatable": 1,
@@ -92,21 +92,21 @@ def get_quality_feedback_custom_fields():
 				"insert_after": "custom_noticed_damage"
 			},
 			{
-				"fieldname": "damage_attachment",
+				"fieldname": "custom_damage_attachment",
 				"fieldtype": "Attach Image",
 				"label": "Damage Attachment",
 				"depends_on": "eval:doc.custom_noticed_damage == \"Yes\"",
 				"insert_after": "custom_damage_description"
 			},
 			{
-				"fieldname": "feedback",
+				"fieldname": "custom_feedback",
 				"fieldtype": "Small Text",
 				"label": "Feedback",
 				"translatable": 1,
 				"insert_after": "custom_damage_attachment"
 			},
 			{
-				"fieldname": "quality_score_percentage",
+				"fieldname": "custom_quality_score_percentage",
 				"fieldtype": "Float",
 				"label": "Quality Score Percentage",
 				"read_only": 1,

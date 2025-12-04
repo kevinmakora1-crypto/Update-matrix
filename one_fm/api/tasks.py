abcdfbf9b0aa1ea58081f18d372ebf6c4e0665bd
NoBoneZ <adekunleabraham09@gmail.com>
2025-12-04 08:12:16 +0100

    reverted comment

--- one_fm/api/tasks.py
+++ one_fm/api/tasks.py
@@ -1781,8 +1781,8 @@ def fetch_employees_not_in_checkin():
 	attendance request, shift request, leave application based on log_type and
 	if their shift start or end falls within the past 1 hour
 	"""
-	# if not production_domain():
-	# 	return
+	if not production_domain():
+		return
 	# if not frappe.db.get_single_value('HR and Payroll Additional Settings', 'remind_employee_checkin_checkout') and not production_domain():
 	# 	return
 

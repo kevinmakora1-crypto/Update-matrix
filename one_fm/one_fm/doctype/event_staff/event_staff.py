e3fc7c303c0924c73b8dff74095f90a12d428251
jam <jam@jams-MacBook-Air.local>
2025-12-07 22:30:13 +0530

    feat: Employee Schedule from Event Staff

--- one_fm/one_fm/doctype/event_staff/event_staff.py
+++ one_fm/one_fm/doctype/event_staff/event_staff.py
@@ -11,7 +11,6 @@ class EventStaff(Document):
 		self.validate_date()
 		self.validate_staffing_requirement()
 		self.validate_overlapping_event_staff()
-		self.validate_overlapping_schedules()
 
 	def validate_date(self):
 		if getdate(self.end_date) < getdate(self.start_date):
@@ -75,51 +74,30 @@ class EventStaff(Document):
 			msg = f"{self.employee} already assigned to {self.designation} from {overlapping[0]['start_date']} to {overlapping[0]['end_date']}."
 			frappe.throw(msg)
 
-	def validate_overlapping_schedules(self):
-		if not self.employee or not self.start_date or not self.end_date:
-			return
-		overlapping_schedules = frappe.get_all(
-			"Employee Schedule",
-			filters={
-				"employee": self.employee,
-				"docstatus": 1,
-				"date": ["between", [self.start_date, self.end_date]]
-			}
-		)
-		if overlapping_schedules:
-			frappe.throw(f"{self.employee} already scheduled during the selected date range")
-
 	def on_submit(self):
-		self.create_employee_schedule()
+		self.process_employee_schedules()
 
-	def create_employee_schedule(self):
-		start_date = getdate(self.start_date)
+	def process_employee_schedules(self, start_date=None):
+		start_date = getdate(start_date) if start_date else getdate(self.start_date)
 		end_date = getdate(self.end_date)
-		number_of_days = date_diff(end_date, start_date) + 1  # Include end date
+		existing_schedules = get_existing_schedules(self.employee, start_date, end_date)
+		existing_schedules_map = { frappe.utils.getdate(d.date): d.name for d in existing_schedules }
+
+		number_of_days = date_diff(end_date, start_date) + 1
+
 		for i in range(number_of_days):
-			date = add_days(start_date, i)
-			start_end_datetime = self.get_event_start_end_date_time(date)
-			employee_schedule = frappe.get_doc(
-				{
-					"doctype": "Employee Schedule",
-					"employee": self.employee,
-					"date": date,
-					"shift": self.operations_shift,
-					"shift_type": self.shift_type,
-					"employee_availability": 'Working',
-					"roster_type": self.roster_type,
-					"site": self.site,
-					"project": self.project,
-					"reference_doctype": self.doctype,
-					"reference_docname": self.name,
-					"is_event_schedule": 1,
-					"event_staff": self.name,
-					"day_off_ot": self.day_off_ot,
-					"start_datetime": start_end_datetime.get("start_datetime"),
-					"end_datetime": start_end_datetime.get("end_datetime")
-				}
-			)
-			employee_schedule.save(ignore_permissions=True)
+			current_date = add_days(start_date, i)
+			start_end_datetime = self.get_event_start_end_date_time(current_date)
+			employee_schedule_data = self.get_schedule_data(start_end_datetime, current_date)
+
+			if current_date in existing_schedules_map:
+				schedule_doc = frappe.get_doc("Employee Schedule", existing_schedules_map[current_date])
+				schedule_doc.update(employee_schedule_data)
+				schedule_doc.save(ignore_permissions=True)
+			else:
+				new_schedule = frappe.new_doc("Employee Schedule")
+				new_schedule.update(employee_schedule_data)
+				new_schedule.insert(ignore_permissions=True)
 
 	def get_event_start_end_date_time(self, date=None):
 		start_time = frappe.utils.get_datetime(self.start_datetime).time()
@@ -135,10 +113,41 @@ class EventStaff(Document):
 			"end_datetime": end_datetime
 		}
 
+	def get_schedule_data(self, start_end_datetime, current_date):
+		return {
+			"employee": self.employee,
+			"date": current_date,
+			"employee_availability": "Client Event",
+			"is_event_schedule": 1,
+			"event_staff": self.name,
+			"shift": self.operations_shift,
+			"shift_type": self.shift_type,
+			"project": self.project,
+			"site": self.site,
+			"start_datetime": start_end_datetime.get("start_datetime"),
+			"end_datetime": start_end_datetime.get("end_datetime"),
+			"designation": self.designation,
+			"roster_type": self.roster_type,
+			"day_off_ot": self.day_off_ot,
+			"client_event": self.client_event,
+			"event_location": self.event_location,
+			"reference_doctype": "Event Staff",
+			"reference_docname": self.name,
+			"operations_role": None,
+			"post_abbrv": None,
+		}
+
 	def on_update_after_submit(self):
 		old_doc = self.get_doc_before_save()
 		if getdate(self.end_date) < getdate(old_doc.end_date):
 			self.delete_employee_schedules(self.end_date)
+		elif getdate(self.end_date) > getdate(old_doc.end_date):
+			new_start_date = add_days(getdate(old_doc.end_date), 1)
+			conflicting_dates = get_conflicting_dates(self.employee, new_start_date, self.end_date)
+			if conflicting_dates:
+				frappe.throw(f"Cannot extend the event as there are conflicting schedules on the following dates: {', '.join(conflicting_dates)}")
+
+			self.process_employee_schedules(start_date=new_start_date)
 
 	def delete_employee_schedules(self, date):
 		frappe.db.delete(
@@ -151,3 +160,23 @@ class EventStaff(Document):
 
 	def on_cancel(self):
 		self.delete_employee_schedules(today())
+
+@frappe.whitelist()
+def get_conflicting_dates(employee, start_date, end_date):
+	if not employee or not start_date or not end_date:
+		return []
+
+	conflicting_schedules = get_existing_schedules(employee, start_date, end_date)
+
+	return [schedule.get("date").strftime("%Y-%m-%d") for schedule in conflicting_schedules]
+
+def get_existing_schedules(employee, start_date, end_date):
+	return frappe.get_all(
+		"Employee Schedule",
+		filters={
+			"employee": employee,
+			"docstatus": ("!=", 2),
+			"date": ["between", [start_date, end_date]]
+		},
+		fields=["name", "date"]
+	)

96f5b92a449d747f74a67e3f0f5798e0320cd938
NoBoneZ <adekunleabraham09@gmail.com>
2025-12-14 10:33:25 +0100

    added the defualt user roles

--- one_fm/public/js/doctype_js/purchase_receipt.js
+++ one_fm/public/js/doctype_js/purchase_receipt.js
@@ -15,7 +15,14 @@ frappe.ui.form.on('Purchase Receipt Item', {
 });
 
 function set_readonly_for_warehouse_users(frm) {
-    const warehouse_roles = [
+    const default_roles = [
+        'Assignee',
+        'Penalty Recipient',
+        'Employee',
+        'Employee Self Service'
+    ];
+    
+    const warehouse_stock_roles = [
         'Warehouse Maintainer',
         'Warehouse Supervisor',
         'Stock Issuer',
@@ -31,27 +38,22 @@ function set_readonly_for_warehouse_users(frm) {
         'distributed_discount_amount'
     ];
     
-    const user_roles = frappe.user_roles;
+    const user_roles = frappe.user_roles.filter(role => 
+        !default_roles.includes(role)
+    );
     
-    const has_only_warehouse_roles = user_roles.some(role => 
-        warehouse_roles.includes(role)
-    ) && !user_roles.includes('Accounts Manager') 
-       && !user_roles.includes('Accounts User')
-       && !user_roles.includes('Purchase Manager')
-       && !user_roles.includes('Purchase User');
+    const has_warehouse_role = user_roles.some(role => 
+        warehouse_stock_roles.includes(role)
+    );
     
-    if (has_only_warehouse_roles) {
-        frm.fields_dict.items.grid.update_docfield_property(
-            readonly_fields,
-            'read_only',
-            1
-        );
-        
-        frm.fields_dict.items.grid.fields_map.rate.df.read_only = 1;
-        frm.fields_dict.items.grid.fields_map.amount.df.read_only = 1;
-        frm.fields_dict.items.grid.fields_map.discount_percentage.df.read_only = 1;
-        frm.fields_dict.items.grid.fields_map.discount_amount.df.read_only = 1;
-        frm.fields_dict.items.grid.fields_map.distributed_discount_amount.df.read_only = 1;
+    if (has_warehouse_role) {
+        readonly_fields.forEach(field => {
+            frm.fields_dict.items.grid.update_docfield_property(
+                field,
+                'read_only',
+                1
+            );
+        });
         
         frm.refresh_field('items');
     }

c98cc7f19a82ade3c3d7a27457847ee02d93a428
NoBoneZ <adekunleabraham09@gmail.com>
2025-12-16 07:53:17 +0100

    updated roster

--- one_fm/one_fm/page/roster/roster.py
+++ one_fm/one_fm/page/roster/roster.py
@@ -223,6 +223,8 @@ def get_roster_view(start_date, end_date, employee_search_id=None, employee_sear
 			post_map_filters["site"] = site
 		if shift:
 			post_map_filters["shift"] = shift
+
+		print(post_map_filters, "=================post_map_filters===================", "\n\n\n\n")
 		post_map = PostMap(start=start_date, end=end_date, operations_roles_list=operations_roles, filters=post_map_filters)
 		master_data["operations_roles_data"] = post_map.template
 

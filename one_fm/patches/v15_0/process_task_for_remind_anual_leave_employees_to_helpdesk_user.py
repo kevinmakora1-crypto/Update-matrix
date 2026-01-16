2709da8e4b8b201cdcd33410e9f7508fcdfcd80d
jam <jam@jams-MacBook-Air.local>
2026-01-16 11:46:22 +0530

    feat: send reminder to helpdesk on anual leave Resumption Date

---
+++ one_fm/patches/v15_0/process_task_for_remind_anual_leave_employees_to_helpdesk_user.py
@@ -0,0 +1,18 @@
+import frappe
+from frappe.utils import today
+from one_fm.utils import create_process_task
+
+def execute():
+    create_process_task(
+        "Leave Management",  # process_name
+        "Leave Application",  # erp_document
+        "Remind helpdesk user on leave application",  # task_description
+        method="one_fm.overrides.leave_application.remind_anual_leave_employees_to_helpdesk_user",
+        frequency="Cron",
+        cron_format="* 8 * * *",
+        process_owner=None,
+        business_analyst=None,
+        task_type="Routine",
+        is_routine_task=1,
+        is_automated=1
+    )

059c336bf1ec0761f6b84522323bbc0d3223c5e3
zeinaremah <zeinaremah@gmail.com>
2026-03-01 12:26:32 +0300

    chore: change vehicle patch to use property setter instead of setting value in db directly

--- one_fm/custom/property_setter/vehicle.py
+++ one_fm/custom/property_setter/vehicle.py
@@ -13,5 +13,54 @@ def get_vehicle_properties():
             "property": "quick_entry",
             "property_type": "Check",
             "value": "0"
-        }
+        },
+        {
+            "doctype": "Vehicle",
+            "fieldname": "employee",
+            "property": "reqd",
+            "value": "1",
+            "property_type": "Check",
+        },
+        {
+            "doctype": "Vehicle",
+            "fieldname": "employee",
+            "property": "fieldtype",
+            "value": "Link",
+            "property_type": "Select",
+        },
+        {
+            "doctype": "Vehicle",
+            "fieldname": "employee",
+            "property": "options",
+            "value": "Employee",
+            "property_type": "Small Text",
+        },
+        {
+            "doctype": "Vehicle",
+            "fieldname": "location",
+            "property": "label",
+            "value": "Vehicle Location",
+            "property_type": "Data",
+        },
+        {
+            "doctype": "Vehicle",
+            "fieldname": "location",
+            "property": "fieldtype",
+            "value": "Link",
+            "property_type": "Select",
+        },
+        {
+            "doctype": "Vehicle",
+            "fieldname": "location",
+            "property": "options",
+            "value": "Location",
+            "property_type": "Small Text",
+        },
+        {
+            "doctype": "Vehicle",
+            "fieldname": "location",
+            "property": "reqd",
+            "value": "1",
+            "property_type": "Check",
+        },
     ]
--- one_fm/patches/v15_0/adjust_vehicle_fields.py
+++ one_fm/patches/v15_0/adjust_vehicle_fields.py
@@ -1,43 +1,6 @@
 import frappe
 from one_fm.setup.setup import add_property_setter
+from one_fm.custom.property_setter.vehicle import get_vehicle_properties
 
 def execute():
-    properties = [
-        {
-            "doctype": "Vehicle",
-            "fieldname": "employee",
-            "property": "reqd",
-            "value": "1",
-            "property_type": "Check",
-        },
-        {
-            "doctype": "Vehicle",
-            "fieldname": "location",
-            "property": "label",
-            "value": "Vehicle Location",
-            "property_type": "Data",
-        },
-        {
-            "doctype": "Vehicle",
-            "fieldname": "location",
-            "property": "fieldtype",
-            "value": "Link",
-            "property_type": "Select",
-        },
-        {
-            "doctype": "Vehicle",
-            "fieldname": "location",
-            "property": "options",
-            "value": "Location",
-            "property_type": "Small Text",
-        },
-        {
-            "doctype": "Vehicle",
-            "fieldname": "location",
-            "property": "reqd",
-            "value": "1",
-            "property_type": "Check",
-        },
-    ]
-    add_property_setter(properties)
-    frappe.clear_cache(doctype="Vehicle")
+    add_property_setter(get_vehicle_properties())

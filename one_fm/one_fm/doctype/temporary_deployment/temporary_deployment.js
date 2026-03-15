61a80239ba78b23055c2b60b88fb9885254f9664
jam <jam@jams-MacBook-Air.local>
2026-03-15 12:19:13 +0530

    feat: temporary deployment - links

--- one_fm/one_fm/doctype/temporary_deployment/temporary_deployment.js
+++ one_fm/one_fm/doctype/temporary_deployment/temporary_deployment.js
@@ -1,8 +1,22 @@
 // Copyright (c) 2026, ONE FM and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Temporary Deployment", {
-// 	refresh(frm) {
+frappe.ui.form.on("Temporary Deployment", {
+    refresh(frm) {
+        // Prevent creation of Temporary Post from the Connections badge (+) button
+        frm.sidebar.linked_with && frm.sidebar.linked_with.wrapper
+            && frm.sidebar.linked_with.wrapper
+                .find('[data-doctype="Temporary Post"] .btn-new')
+                .hide();
+    },
 
-// 	},
-// });
+    onload_post_render(frm) {
+        // Also hide after the connections panel fully renders
+        setTimeout(() => {
+            frm.sidebar.linked_with && frm.sidebar.linked_with.wrapper
+                && frm.sidebar.linked_with.wrapper
+                    .find('[data-doctype="Temporary Post"] .btn-new')
+                    .hide();
+        }, 500);
+    }
+});

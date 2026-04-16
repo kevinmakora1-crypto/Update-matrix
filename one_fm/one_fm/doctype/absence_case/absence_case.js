f0e9c8e3aa8b1edebfd954f92ef7e5a6aa6fba66
NoBoneZ <adekunleabraham09@gmail.com>
2026-04-16 10:38:43 +0100

    added query for leave application and logic for leave extension button

--- one_fm/one_fm/doctype/absence_case/absence_case.js
+++ one_fm/one_fm/doctype/absence_case/absence_case.js
@@ -1,8 +1,23 @@
 // Copyright (c) 2026, ONE FM and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Absence Case", {
-// 	refresh(frm) {
+frappe.ui.form.on("Absence Case", {
+	refresh(frm) {
+		frm.set_query("leave_application", function() {
+			return {
+				filters: {
+					"leave_type": "Annual Leave"
+				}
+			};
+		});
 
-// 	},
-// });
+		if (frm.doc.docstatus === 1 && frm.doc.received_leave_extension_request === "Yes" && frm.doc.leave_application) {
+			frm.add_custom_button(__("Leave Extension Request"), function() {
+				frappe.new_doc("Leave Extension Request", {
+					"employee": frm.doc.employee,
+					"leave_application": frm.doc.leave_application
+				});
+			}, __("Create"));
+		}
+	},
+});

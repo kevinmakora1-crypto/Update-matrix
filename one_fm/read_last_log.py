43842f6beadc4bf92c856be873832efbeee2019f
Kevin Makora <develop@one-fm.com>
2026-04-27 10:22:14 +0300

    chore: remove ad-hoc diagnostic script read_last_log.py

--- one_fm/read_last_log.py
+++
@@ -1,7 +0,0 @@
-import frappe
-def run():
-    log = frappe.db.get_list('Error Log', fields=['method', 'error'], order_by='creation desc', limit=1)
-    if log:
-        print(f"METHOD: {log[0].method}")
-        print(f"ERROR: {log[0].error}")
-run()
